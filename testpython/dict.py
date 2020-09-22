5b1ddbea53939386ef0ea463933d0505504987cf
EC2 Default User <ec2-user@ip-172-31-3-101.ap-south-1.compute.internal>
2020-09-22 11:01:21 +0000

    dict and tuple files r added

---
+++ testpython/dict.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+dict = {'name': 'Ravi','Age':8, 'class': 'Second'}
+
+print("Value of ['name'] ==", dict['name'])
---
+++ testpython/tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+tup = ('phisics', 'chemistry', 1, 2, 3, 4);
+print(tup)
+del tup;
+print "after deleting tuple";
+print tup;
